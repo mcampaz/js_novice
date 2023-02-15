954d774f60c3dd4d6e538f6c80db2271a75a085d
MARIO CAMPAZ <mcampaz@users.noreply.github.com>
2023-02-14 22:55:55 -0800

    [add] Quiz functionality

--- c2_basics/index.js
+++ c2_basics/index.js
@@ -2,11 +2,25 @@ const btn = document.getElementById('button');
 
 const rainbow = ['red', 'orange', 'yellow', 'green', 'blue','rebeccapurple', 'violet'];
 
-alert("Welcome to Quiz!!!")
-
 function change() {
-    document.body.style.background = rainbow[Math.floor(7*Math.random())];    const message = "My number... infinite of Hello World!!!";
-    alert(message);
+    document.body.style.background = rainbow[Math.floor(7*Math.random())];   
+
+    const quiz = [
+        ["What is Superman's real name?","Clark Kent"],
+        ["What is Wonder Woman's real name?","Diana Prince"],
+        ["What is Batman's real name?","Bruce Wayne"]
+    ];
+    
+    let score = 0;
+    for(const [question, answer] of quiz) {
+        const answered = prompt(`${question}`);
+        const result = (answered === answer);
+        if (result) {
+            score++;
+        }
+        alert(`Your answer is: ${result}`);
+    }
+    alert(`Your score is: ${score}`);
 }
 
 btn.addEventListener('click', change);
