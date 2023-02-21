49e4ad6687e0626ff1fe0f8125cd1e7e80deb105
MARIO CAMPAZ <mcampaz@users.noreply.github.com>
2023-02-21 10:01:43 -0800

    [Refactor] Breaking down code to small array functions

--- c4_functions/index.js
+++ c4_functions/index.js
@@ -5,6 +5,23 @@ const rainbow = ['red', 'orange', 'yellow', 'green', 'blue','rebeccapurple', 'vi
 function change() {
     document.body.style.background = rainbow[Math.floor(7*Math.random())];   
 
+    const ask = (question, answer) => {
+        const answered = prompt(`${question}`);
+        check(answer, answered);
+    }
+    
+    const check = (answer, answered) => {
+        const result = (answered === answer);
+        if (result) {
+            score++;
+        }
+        alert(`Your answer is: ${result} and Your score is: ${score}`);
+    }
+    
+    const gameOver = () => {
+        alert(`Game Over!!! Your score is: ${score}`);
+    }
+    
     const quiz = [
         ["What is Superman's real name?","Clark Kent"],
         ["What is Wonder Woman's real name?","Diana Prince"],
@@ -12,15 +29,40 @@ function change() {
     ];
     
     let score = 0;
-    for(const [question, answer] of quiz) {
-        const answered = prompt(`${question}`);
-        const result = (answered === answer);
-        if (result) {
-            score++;
-        }
-        alert(`Your answer is: ${result}`);
-    }
-    alert(`Your score is: ${score}`);
+
+    quiz.forEach((value) => 
+        ask(value[0], value[1]));
+
+    gameOver();
+
+    
+    
 }
 
 btn.addEventListener('click', change);
+
+function testMean() {
+    
+    // function mean(array) {
+    //     const total = array.reduce((a, b) => a + b);
+    //     return total/array.length;
+    // }
+
+    function mean(array, callback) {
+        if (callback) {
+            array.map( callback );
+        }
+        const total = array.reduce((a, b) => a + b);
+        return total/array.length;
+    }
+
+    function double(number){
+        return 2 * number;
+    }
+
+    let myNumbers = [1, 2, 3, 4, 5];
+
+    console.log("Doubled; Mean: " + mean(myNumbers, x => 2 * x));
+
+}
+testMean();
